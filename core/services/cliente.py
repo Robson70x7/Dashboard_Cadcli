e7c44c25d6af16bcdc69b961a15878a46ff1e9b4
Robson Silva Brito de Moraes <robson.silva94@hotmail.com>
2019-03-27 16:19:52 -0300

    Fix bug

--- core/services/cliente.py
+++ core/services/cliente.py
@@ -12,6 +12,7 @@ class ServiceClient():
         self.email = ''
         self.data_nascimento = ''
 
+
     def verify_exist_email(self, email):
         lista = Client.objects.filter(email__exact = email)
         if lista:
@@ -26,9 +27,23 @@ class ServiceClient():
         return -> Retorna todos clientes.
         """
         return Client.objects.all()
+
     
     def find(self, id):
         return get_object_or_404(Client, id=id)
+
+
+    def create_auto(self, id, kwargs):
+        client = self.find(id)
+        auto = client.automovel_set.create(
+            marca=kwargs['marca'], modelo=kwargs['modelo'],
+            cor=kwargs['cor'], ano=kwargs['ano']
+        )
+        if auto:
+            return True
+        else:
+            False
+
 
     def create(self):
         """
@@ -48,6 +63,15 @@ class ServiceClient():
         return new_client
 
 
+    def filter(**kwarg):
+        return get_object_or_404(Client, kwarg)
+
+
+    @property
+    def latest(self):
+        return Client.objects.latest('id')
+
+
     def delete(self, id):
         client = self.find(id)
         if client:
@@ -55,6 +79,7 @@ class ServiceClient():
             return True
         
         return False
+
 
     def update(self, id):
         client = self.find(id=id)
--- core/views.py
+++ core/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
-from .services.cliente import ServiceClient
+from  .services.cliente import ServiceClient
 from django.views.decorators import http
 from django.http import HttpResponse, HttpResponseRedirect, QueryDict
 from datetime import datetime
 from django.urls import reverse
 from .forms import ClientForm, AutomovelForm
-import json
+
+service_client = ServiceClient()
 
 @http.require_GET
 def index(request):
-    cli = ServiceClient()
-    clients = cli.get_all()
+    clients = service_client.get_all()
     context_view = {'clients':clients}
-
+    
     if 'messages' in request.session:
         messages = request.session.pop('messages')
         context_view.update(messages)
@@ -23,28 +23,42 @@ def index(request):
 def create(request):
     """Criar novo usuario """
     form = ClientForm()
-
     #Implementar o form de Automovel
     form_auto = AutomovelForm()
-
     context_view = {'form':form, 'form_auto':form_auto}
     
     if request.method == 'POST':
-        form_preenchido = ClientForm(dados_client)
-        
+        dispose_session(request)
+
+        dados = request.POST
+        fields_auto = ['client_id','marca','modelo','ano','cor']
+        dados_client = {key:value for key,value in dados.items() if key not in fields_auto}
+        dados_auto = {key:value for key,value in dados.items() if key in fields_auto}
+
+        form_preenchido = ClientForm(dados_client or None)
+
         if form_preenchido.is_valid():
             #Verificações
-            if ServiceClient().verify_exist_email(request.POST['email']):
+            if service_client.verify_exist_email(dados['email']):
                 request.session['messages'] = {'error_message':'Este email ja existe em nossa base de dados, corrija-o e tente novamente'}
                 context_view['form'] = form_preenchido
                 context_view.update(request.session.pop('messages'))
                 return render(request, 'core/create.html', context= context_view)
-           
-            form_preenchido.save()
-            request.session['messages'] = {'success_message':'Cliente cadastrado com sucesso'}
+
+            new_client = form_preenchido.save()
         else:
-            request.session['messages'] = {'error_message':'O formulario enviado esta com inválido, preencha novamente e envie'}
+            request.session['messages'] = {'error_message':'O formulario enviado esta inválido, preencha novamente e envie'}
             return redirect('core:create')
+
+        #Formulario de Automovel
+        #client = service_client.latest  # recupera ultimo dado cadastrado
+        cad_auto = service_client.create_auto(new_client.id, dados_auto)
+
+        if not cad_auto:
+            request.session['messages'] = {'error_message':'Cliente cadastrado, porém, Automovel Não cadastrou'}
+        else:
+            request.session['messages'] = {'success_message':'Cliente, e automovel cadastrado com sucesso'}
+        
     
     if 'messages' in request.session:
         message = request.session.pop('messages')
@@ -54,7 +68,8 @@ def create(request):
 
 
 def edit(request, client_id):
-    client = ServiceClient().find(client_id)
+    dispose_session(request)
+    client = service_client.find(client_id)
     form = ClientForm(request.POST or None, instance=client)
     context_view = {'form':form, 'client_id':client.id}
     
@@ -74,10 +89,9 @@ def edit(request, client_id):
 
 
 def delete(request, client_id):
-    if 'messages' in request.session:
-        request.session.flush()
+    dispose_session(request)
 
-    if ServiceClient().delete(client_id):
+    if service_client.delete(client_id):
         request.session['messages'] = {'success_message':'Cliente excluido com sucesso'}
         return redirect('/')
     else:
@@ -86,7 +100,11 @@ def delete(request, client_id):
 
 
 def detail (request, client_id):
-    client = ServiceClient()
+    client = service_client
     result = client.find(id= client_id)
 
     return render(request, 'core/detail.html', {'client':result})
+
+def dispose_session(request):
+    if 'messages' in request.session:
+        request.session.flush()
